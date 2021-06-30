d5dc9eaa28ea7b85ae097b7fead164224db808d0
MAbdulWasey <awasey12@gmail.com>
2021-06-30 14:20:44 +0500

    add a comment

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,13 +3,14 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
+// its is task 
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
     
     @Get()
-    getTasks(@Query filterDto: GetTasksFilterDto): Task[]{
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[]{
         if(Object.keys(filterDto).length){
             return this.tasksService.getTasksWithFilters(filterDto);
         }else{
